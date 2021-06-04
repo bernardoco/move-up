fe94b4ba85c910cd05b6efd62dbb815953acfc07
bernardoco <bernardoco@poli.ufrj.br>
2021-06-04 17:54:46 -0300

    Fix join event crash

--- frontend/src/components/JoinEvent.js
+++ frontend/src/components/JoinEvent.js
@@ -2,6 +2,7 @@ import { useCallback, useState, useContext  } from 'react'
 import { UserContext } from "../providers/UserProvider";
 import { navigate } from "@reach/router";
 import {auth} from "../firebase";
+import ShowParticipants from './ShowParticipants'
 
 
 const JoinEvent = ( { onJoin, setState, _id, participants} ) => {
@@ -11,16 +12,7 @@ const JoinEvent = ( { onJoin, setState, _id, participants} ) => {
 
     return (
         <form className='add-form' onSubmit={() => onJoin(displayName, uid)}>
-            <div>
-                <h4>Participants:</h4>
-                {participants.map((participant) => (
-                        <p key={participant}>- {participant}</p>
-                    ))}           
-            </div>
-            {/* <div className='form-control'>
-                <h2 className = "text-2xl font-semibold">{displayName}</h2>
-            </div> */}
-
+            <ShowParticipants participants={participants} />
             <input type='submit' value='Join Event' className='button button-block' />
         </form>
     )
